bb23c10ae3b18b3ee0e684e4197d2bdd7f2ab0a8
Ese Erigha <ese.erigha@gmail.com>
2019-09-11 00:39:44 +0100

    Added post schema validation on user mutations

--- src/graphiql/resolvers/user.ts
+++ src/graphiql/resolvers/user.ts
@@ -1,30 +1,50 @@
+import { UserInputError, AuthenticationError } from 'apollo-server';
+import {validate} from "@hapi/joi";
 import { ICreateUserInputType, IAuth, IUser, ILoginInputType } from "../interface";
 import { userService } from "../../services";
 import { generateToken, validPassword } from "../../utils/auth";
+import {createUserSchema, loginSchema} from "../../validation/user.validator";
 
 export default{
     Mutation: {
         createUser: async(root, input: ICreateUserInputType): Promise<IAuth> => {
-            let user: IUser = await userService.findOne({name: input.name});
-            if(!user){
-                user = await userService.create(input);
-                const token = await generateToken(user);
-                return {token,user};
-            }
-            throw new Error("User already exist");
-        },
-        login: async(root, input: ILoginInputType): Promise<IAuth> => {
 
-            let user: IUser = await userService.findOne({email: input.email});
-
-            if(user){
-                const isValidPassword: Boolean = await validPassword(input.password,user.password);
-                if(isValidPassword){
+            const {error} = validate(input, createUserSchema);
+            if(!error){
+                let user: IUser = await userService.findOne({name: input.name});
+                if(!user){
+                    user = await userService.create(input);
                     const token = await generateToken(user);
                     return {token,user};
                 }
+                throw new UserInputError ("User already exist");
             }
-            throw new Error("Invalid username or password");
+            throw new UserInputError(
+                'Failed due to validation errors',
+                {validationError: error}
+            );
+            
+        },
+        login: async(root, input: ILoginInputType): Promise<IAuth> => {
+
+            const {error} = validate(input, loginSchema);
+            if(!error){
+
+                let user: IUser = await userService.findOne({email: input.email});
+
+                if(user){
+                    const isValidPassword: Boolean = await validPassword(input.password,user.password);
+                    if(isValidPassword){
+                        const token = await generateToken(user);
+                        return {token,user};
+                    }
+                }
+                throw new AuthenticationError("Invalid username or password");
+            }
+            throw new UserInputError(
+                'Failed due to validation errors',
+                {validationError: error}
+            );
         }
     }
 };
