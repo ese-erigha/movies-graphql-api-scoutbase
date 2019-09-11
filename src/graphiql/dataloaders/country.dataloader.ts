f9c5130f80ee0def022d2cc51d794d65ae121f45
Ese Erigha <ese.erigha@gmail.com>
2019-09-11 02:55:24 +0100

    export dataloaders

--- src/graphiql/dataloaders/country.dataloader.ts
+++ src/graphiql/dataloaders/country.dataloader.ts
@@ -5,5 +5,5 @@ const findCountriesByIds = async(ids: number[])=>{
     return await models.Country.findAll({where: {id: ids}}).map(country => country.get({plain: true})); 
 };
 
-
 export const batchFindCountriesLoader = new DataLoader(findCountriesByIds);
+
