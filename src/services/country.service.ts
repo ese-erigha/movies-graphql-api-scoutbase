cea53d700bb810ec91e3f7e3bff40817e7bc4ac2
Ese Erigha <ese.erigha@gmail.com>
2019-09-11 02:53:49 +0100

    Move dataloader in country service

--- src/services/country.service.ts
+++ src/services/country.service.ts
@@ -1,6 +1,8 @@
 const models = require("../database/models");
 import modelFactory from "./model.factory";
+import {batchFindCountriesLoader} from "../graphiql/dataloaders/country.dataloader";
 
 export default {
-    ...modelFactory(models.Country)
+    ...modelFactory(models.Country),
+    batchFindCountriesLoader
 };
